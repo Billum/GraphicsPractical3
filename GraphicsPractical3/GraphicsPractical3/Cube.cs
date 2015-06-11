f87ce0d9025a4de5ef72087255e0cff4ec9d74fd
Adriaan Kisjes <a.t.kisjes@gmail.com>
2015-06-11 19:20:57 +0200

    gerwins methode verplaatst naar logische plek

--- GraphicsPractical3/GraphicsPractical3/Cube.cs
+++ GraphicsPractical3/GraphicsPractical3/Cube.cs
@@ -60,25 +60,5 @@ namespace Geometry
         }
     }
 
-    public class Screen
-    {
-        public int Width;
-        public int Height;
-    }
-
-    public class Engine
-    {
-        public void Update(Vector3 e, Screen s)
-        {
-            for (uint i = 0; i < s.Width; i++)
-            {
-                for (uint j = 0; j < s.Height; j++)
-                {
-                    // perspective view ray construction
-                    Vector3 d = new Vector3(s.Width/2, 0 /*?z=?*/, s.Height/2) - e;
-                    var r = new Ray(-d *  , e);
-                }
-            }
-        }
-    }
+    
 }
