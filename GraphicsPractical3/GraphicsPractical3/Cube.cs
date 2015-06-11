eed9e5b7e3f5f7ce8025b78f4c3b34999cf41f82
Gerwin vdL aka gerwin3 <gerwin3@gmail.com>
2015-06-11 16:15:50 +0200

    perspective ray construction

--- GraphicsPractical3/GraphicsPractical3/Cube.cs
+++ GraphicsPractical3/GraphicsPractical3/Cube.cs
@@ -21,6 +21,7 @@ namespace Geometry
             float a = Vector3.Dot(v, ray.Direction);
         }
     }
+
     class Ray
     {
         public Vector3 Direction;
@@ -29,6 +30,28 @@ namespace Geometry
         {
             this.Direction = direction;
             this.Origin = origin;
+        }
+    }
+
+    public class Screen
+    {
+        public int Width;
+        public int Height;
+    }
+
+    public class Engine
+    {
+        public void Update(Vector3 e, Screen s)
+        {
+            for (uint i = 0; i < s.Width; i++)
+            {
+                for (uint j = 0; j < s.Height; j++)
+                {
+                    // perspective view ray construction
+                    Vector3 d = new Vector3(s.Width/2, 0 /*?z=?*/, s.Height/2) - e;
+                    var r = new Ray(-d *  , e);
+                }
+            }
         }
     }
 }
