2136d2e8c0670cd9b09ecb6bd03fde5ed304eb6e
Adriaan Kisjes <a.t.kisjes@gmail.com>
2015-06-17 11:25:40 +0200

    Models class om alle models van Game1.cs te scheiden om merge conficts te voorkomen

--- GraphicsPractical3/GraphicsPractical3/Game1.cs
+++ GraphicsPractical3/GraphicsPractical3/Game1.cs
@@ -16,6 +16,7 @@ using PointLight = GraphicsPractical3.Geometry.PointLight;
 using Engine = GraphicsPractical3.RayTracing.Engine;
 using Eye = GraphicsPractical3.RayTracing.Eye;
 using Screen = GraphicsPractical3.RayTracing.Screen;
+using Models = GraphicsPractical3.Models;
 
 namespace GraphicsPractical3
 {
@@ -68,18 +69,10 @@ namespace GraphicsPractical3
             this.pixels = new Color[307200];
             this.eye = new Eye(new Vector3(0, 0, 0), new Vector3(0, 0, 1), 1f);
             this.screen = new Screen(640, 480, 0.001f);
-            this.pointLights = new PointLight[1];
-            this.primitives = new Primitive[2];
             this.texture = new Texture2D(GraphicsDevice, 640, 480);
-
-            this.primitives[0] = new Sphere(new Vector3(0, 0, 10), 1f);
-            this.primitives[0].Color = new Vector3(1f, 0f, 0f);
-
-            this.primitives[1] = new Triangle(new Vector3(-3, -3, 12), new Vector3(-3, 3, 12), new Vector3(3, 3, 12));
-            this.primitives[1].Color = new Vector3(0f, 0f, 1f);
-
-            //this.pointLights[0] = new Light(new Vector3(0, -20, 0), new Vector3(100, 100, 100));
-            this.pointLights[0] = new Light(new Vector3(20, -15, -20), new Vector3(100, 100, 100));
+            Models m = new Models();
+            this.pointLights = m.PointLights();
+            this.primitives = m.Primitives();
 
             this.engine = new Engine(primitives, pointLights);
 
---
+++ GraphicsPractical3/GraphicsPractical3/Models.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Triangle = GraphicsPractical3.Geometry.Triangle;
+using Sphere = GraphicsPractical3.Geometry.Sphere;
+using Light = GraphicsPractical3.Geometry.PointLight;
+using Primitive = GraphicsPractical3.Geometry.Primitive;
+using PointLight = GraphicsPractical3.Geometry.PointLight;
+
+namespace GraphicsPractical3
+{
+    class Models
+    {
+        private Primitive[] primitives;
+        private PointLight[] pointLights;
+        
+        public Models()
+        {
+            this.pointLights = new PointLight[1];
+            // Initiate Point Lights here
+
+            this.pointLights[0] = new Light(new Vector3(20, -15, -20), new Vector3(100, 100, 100));
+
+            this.primitives = new Primitive[2];
+            // Initiate Primitives here
+
+            this.primitives[0] = new Sphere(new Vector3(0, 0, 10), 1f);
+            this.primitives[0].Color = new Vector3(1f, 0f, 0f);
+
+            this.primitives[1] = new Triangle(new Vector3(-3, -3, 12), new Vector3(-3, 3, 12), new Vector3(3, 3, 12));
+            this.primitives[1].Color = new Vector3(0f, 0f, 1f);
+        }
+
+        public Primitive[] Primitives()
+        {
+            return primitives;
+        }
+
+        public PointLight[] PointLights()
+        {
+            return pointLights;
+        }
+    }
+}
